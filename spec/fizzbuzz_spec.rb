bbe88df1f5f0dd3b81428807aa7c06367a17f1d8
Michelle Epstein <epstein.michelle@gmail.com>
2016-09-22 11:07:12 +0000

    fizzbuzz_pair

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,13 @@ describe 'fizzbuzz' do
     it 'returns "fizz" when passed 3' do 
         expect(fizzbuzz(3)).to eq 'fizz'
     end
+    it 'returns "buzz" when passed 5' do 
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+    it 'returns "fizzbuzz" when passed 3 && 5' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+    it 'returns number when passed number !=3 or !5' do
+        expect(fizzbuzz(7)).to eq 7
+    end
 end
